29387e65d9ffa1f7a3e0dfadd2c926d5aca12a16
mathbraga <matheus_ob59@hotmail.com>
2020-12-01 11:35:37 -0300

    Implement r/w functions for strings in assembly.

--- func.s
+++ func.s
@@ -6,77 +6,32 @@ _msg2 db " caracteres.", 0dh, 0ah
 _msg2_s EQU $-_msg2
 
 section .bss
-TMP_DATA resd 2
+TMP_DATA resd 1
 _bufferData resb 5
 _aux resb 5
+_bufferString resb 100
 _result resd 1
 
 section .text
 _start:
-push eax ; C_INPUT
+push eax ; S_INPUT
 push TMP_DATA
-call LeerChar
+push DWORD 8
+call LeerString
 mov [_result], eax
 call _displayNchars
 pop eax
 
-push eax ; C_OUTPUT
-push DWORD [TMP_DATA]
-call EscreverChar
+push eax ; S_OUTPUT
+push TMP_DATA
+push DWORD 8
+call EscreverString
 pop eax
 
 ;------------------------------------
 mov eax, 1
 mov ebx, 0
 int 80h
-
-LeerChar:
-enter 0,0
-mov DWORD [_bufferData], 0
-mov BYTE [_bufferData + 4], 0
-push ecx
-push edx
-push ebx
-mov eax, 3
-mov ebx, 2
-mov ecx, _bufferData
-mov edx, 2
-int 80h
-mov ebx, [EBP + 8]
-mov al, [_bufferData]
-cbw
-cwde
-mov [ebx], eax
-mov eax, 1
-add eax, 0x30
-pop ebx
-pop edx
-pop ecx
-leave
-ret 4
-
-EscreverChar:
-enter 0,0
-mov DWORD [_bufferData], 0
-mov BYTE [_bufferData + 4], 0
-push ecx
-push edx
-push ebx
-mov bl, [EBP + 8]
-mov [_bufferData], bl
-mov [_bufferData + 1], BYTE 0x0a
-mov eax, 4
-mov ebx, 1
-mov ecx, _bufferData
-mov edx, 2
-int 80h
-mov eax, 1
-add eax, 0x30
-pop ebx
-pop edx
-pop ecx
-leave
-ret 4
 
 _displayNchars:
 enter 0,0
